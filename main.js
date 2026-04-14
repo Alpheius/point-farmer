a482e17612f2d1f6cf3667bb1e42bc05e4521129
Alpheus <104544479+Alpheius@users.noreply.github.com>
2026-04-14 03:00:59 -0700

    fixed phasing (for the most part), delete balls, ball hologram

--- main.js
+++ main.js
@@ -47,6 +47,10 @@ const ctx = canvas.getContext("2d")
 let elasticity = 1;
 let friction = 0.99;
 let g = 1;
+let rest = 10;
+
+let mouseX;
+let mouseY;
 
 
 
@@ -60,6 +64,9 @@ class Ball {
         this.density = density;
     }
     index = balls.length;
+    prevX = this.x;
+    prevY = this.y;
+    mouseHover = false;
 
     get mass() {
         return this.r * this.r * Math.PI * this.density;
@@ -82,9 +89,7 @@ class Ball {
             let ball = balls[i];
             let distX = ball.x - this.x;
             let distY = ball.y - this.y;
-            let dist = Math.hypot(distX, distY);
-            // console.log(distX, distY, dist);
-            // console.log(this.x, ball.x);
+            let dist = Math.hypot(distX, distY, 1);
 
             this.vx += g*ball.mass/(dist*dist*dist)*distX*delta;
             this.vy += g*ball.mass/(dist*dist*dist)*distY*delta;
@@ -94,6 +99,9 @@ class Ball {
 
     update(delta) {
         // this.vy += g * delta;
+
+        this.prevX = this.x;
+        this.prevY = this.y;
 
         this.x += this.vx * delta;
         this.y += this.vy * delta;
@@ -111,59 +119,111 @@ class Ball {
 
         this.vx *= Math.pow(friction, delta);
         this.vy *= Math.pow(friction, delta);
+
+        if(Math.abs(this.vx) < 0.01) this.vx = 0;
+        if(Math.abs(this.vy) < 0.01) this.vy = 0;
+    }
+
+    
+    checkCollision(delta) {
+        for(let i = this.index + 1; i < balls.length; i++) {
+            let ball = balls[i];
+            
+            
+            let totalX = this.x - ball.x;
+            let totalY = this.y - ball.y;
+            let totalR = this.r + ball.r;
+            
+            if(totalX*totalX + totalY*totalY <= totalR*totalR) {
+                let dist = Math.hypot(totalX, totalY);
+
+                let m1 = this.mass;
+                let m2 = ball.mass;
+                
+                let nx;
+                let ny;
+                if (dist === 0) {
+                    nx = 1;
+                    ny = 0;
+                    dist = 0.0001;
+                } else {
+                    nx = totalX/dist;
+                    ny = totalY/dist; 
+                }
+                let overlap = totalR - dist;
+                if(overlap > 0) {
+                    let move1 = -overlap*(m2/(m1+m2));
+                    let move2 = -overlap*(m1/(m1+m2));
+                    
+                    this.x -= move1 * nx;
+                    this.y -= ny*move1;
+                    
+                    ball.x += nx*move2;
+                    ball.y += ny*move2;
+                }
+                
+                gameData.collisions += 1;
+                document.getElementById("collisionsMade").innerHTML = gameData.collisions + " Collisions";
+            };
+        }
+    }
+    
+    checkCollision2(delta) {
+        for(let i = this.index + 1; i < balls.length; i++) {
+            let ball = balls[i];
+            
+            
+            let totalX = this.x - ball.x;
+            let totalY = this.y - ball.y;
+            let totalR = this.r + ball.r;
+            
+            if(totalX*totalX + totalY*totalY <= totalR*totalR) {
+                let dist = Math.hypot(totalX, totalY);
+                let m1 = this.mass;
+                let m2 = ball.mass;
+                
+                let nx;
+                let ny;
+                if (dist === 0) {
+                    nx = 1;
+                    ny = 0;
+                    dist = 0.0001;
+                } else {
+                    nx = totalX/dist;
+                    ny = totalY/dist; 
+                }
+                
+                let rvx = this.vx - ball.vx;
+                let rvy = this.vy - ball.vy;
+                
+                let velAlongNormal = rvx * nx + rvy * ny;
+                if (Math.abs(velAlongNormal) < 1) {
+                    velAlongNormal = 0;
+                }
+                if (velAlongNormal > 0) continue;
+                
+                let j =  (1+elasticity) * velAlongNormal/(1/m1 + 1/m2);
+                this.vx -= (j / m1) * nx;
+                this.vy -= (j / m1) * ny;
+                ball.vx += (j / m2) * nx;
+                ball.vy += (j / m2) * ny;
+            }
+        }
     }
 
     draw() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
         ctx.stroke();
-        ctx.fillStyle = `rgb(${255 - this.density/100 * 255}, ${255 - this.density/100 * 255}, ${255 - this.density/100 * 255})`
+        ctx.fillStyle = `rgb(${Math.max(255 - this.density/100 * 255, this.mouseHover * 255)}, ${Math.max(255 - this.density/100 * 255, this.mouseHover * 255)}, ${Math.max(255 - this.density/100 * 255, this.mouseHover * 255)})`
         ctx.fill();
-    }
-
-    checkCollision() {
-        for(let i = this.index + 1; i < balls.length; i++) {
-            let totalX = this.x - balls[i].x;
-            let totalY = this.y - balls[i].y;
-            let totalR = this.r + balls[i].r;
-
-            if(totalX*totalX + totalY*totalY < totalR*totalR) {
-                let dist = Math.hypot(totalX, totalY);
-                
-                // console.log("collision!");
-                let m1 = this.mass;
-                let m2 = balls[i].mass;
-                
-                let nx = totalX/dist;
-                let ny = totalY/dist;
-                
-                let v1n = this.vx * nx + this.vy * ny;
-                let v2n = balls[i].vx * nx + balls[i].vy * ny;
-                
-                let p = v1n - v2n;
-                if (p >= 0) return;
-                let j = (1+elasticity)*p/(m1 + m2);
-                
-                let dv1 = [-j * m2 * nx, -j * m2 * ny];
-                let dv2 = [j * m1 * nx, j * m1 * ny];
-                
-                this.vx += dv1[0];
-                this.vy += dv1[1];
-                balls[i].vx += dv2[0];
-                balls[i].vy += dv2[1];
-
-                gameData.collisions += 1;
-                document.getElementById("collisionsMade").innerHTML = gameData.collisions + " Collisions";
-            };
-        }
     }
 }
 
 
 let balls = [];
-
+let isMouseOverBall = false;
 let last = null;
-
 
 function updateBalls(timestamp) {
     if(!last) last = timestamp;
@@ -172,7 +232,7 @@ function updateBalls(timestamp) {
     let totalMomentum = [0, 0];
     let totalkE = 0;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
+    isMouseOverBall = false;
 
     for(let i = 0; i < balls.length; i++) {
         balls[i].gravitize(delta);
@@ -181,10 +241,32 @@ function updateBalls(timestamp) {
     for(let i = 0; i < balls.length; i++) {
         balls[i].update(delta);
     }
-    for(let i = 0; i < balls.length; i++) {
-        balls[i].checkCollision();
+    for(let j = 0; j < 3; j++) {
+        for(let i = 0; i < balls.length; i++) {
+            balls[i].checkCollision(delta);
+        }
     }
     for(let i = 0; i < balls.length; i++) {
+        balls[i].checkCollision2(delta);
+    }
+    // for(let i = 0; i < balls.length; i++) {
+    //     balls[i].checkCollision(delta);
+    // }
+    // for(let i = 0; i < balls.length; i++) {
+    //     balls[i].checkCollision(delta);
+    // }
+    // for(let i = 0; i < balls.length; i++) {
+    //     balls[i].checkCollision(delta);
+    // }
+    for(let i = 0; i < balls.length; i++) {
+        let ball = balls[i];
+
+        if((ball.x - mouseX)**2 + (ball.y - mouseY)**2 < ball.r**2) {
+            ball.mouseHover = true;
+            isMouseOverBall = true;
+        } else {
+            ball.mouseHover = false;
+        }
         balls[i].draw();
     }
     for(let i = 0; i < balls.length; i++) {
@@ -195,6 +277,26 @@ function updateBalls(timestamp) {
     for(let i = 0; i < balls.length; i++) {
         totalkE += balls[i].kE;
     }
+
+
+    if(!isMouseOverBall) {
+        let x = mouseX;
+        let y = mouseY;
+        let r = parseFloat(document.getElementById("inputR").value);
+        let density = parseFloat(document.getElementById("inputDensity").value);
+
+        ctx.beginPath();
+        ctx.arc(x, y, r, 0, 2*Math.PI);
+        ctx.stroke();
+        ctx.fillStyle = `rgb(${255 - (density+20)/100 * 255}, ${255 - (density+20)/100 * 255}, ${255 - (density+20)/100 * 255})`
+        ctx.fill();
+    }
+    // ctx.beginPath();
+    // ctx.arc(MOU, this.y, this.r, 0, 2*Math.PI);
+    // ctx.stroke();
+    // ctx.fillStyle = `rgb(${Math.max(255 - this.density/100 * 255, this.mouseHover * 255)}, ${Math.max(255 - this.density/100 * 255, this.mouseHover * 255)}, ${Math.max(255 - this.density/100 * 255, this.mouseHover * 255)})`
+    // ctx.fill();
+
     document.getElementById("momentum").innerHTML = "Total Momentum: " + Math.round(totalMomentum[0]*10000)/10000 + ", " + Math.round(totalMomentum[1]*10000)/10000;
     document.getElementById("kineticEnergy").innerHTML = "Total Kinetic Energy (kE): " + Math.round(totalkE*10000)/10000;
 
@@ -217,28 +319,28 @@ function createBallFromInputs() {
 
 
 canvas.addEventListener("click", function(event) {
-    const rect = canvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
+    if(isMouseOverBall) {
+        for(let i = 0; i < balls.length; i++) {
+            let ball = balls[i];
 
-
-    for(let i = 0; i < balls.length; i++) {
-        let ball = balls[i];
-
-        if((ball.x - x)**2 + (ball.y - y)**2 < ball.r**2) {
-            if(Math.abs(ball.vx) + Math.abs(ball.vy) == 0) {
-                ball.vx += 10;
-                ball.vy += 10;
-            } else {
-                ball.vx = 0;
-                ball.vy = 0;
+            if(ball.mouseHover == true) {
+                balls.splice(i, 1);
             }
         }
+    } else {
+        let x = mouseX;
+        let y = mouseY;
+        let r = parseFloat(document.getElementById("inputR").value);
+        let vx = parseFloat(document.getElementById("inputVX").value);
+        let vy = parseFloat(document.getElementById("inputVY").value);
+        let density = parseFloat(document.getElementById("inputDensity").value);
+        balls.push(new Ball(x, y, r, vx, vy, density));
     }
+
 });
 
-balls.push(new Ball(50, 100, 20, 0, 0, 5));
-balls.push(new Ball(150, 100, 20, 0, 0, 5));
+balls.push(new Ball(50, 100, 40, 0, 0, 5));
+balls.push(new Ball(150, 100, 40, 0, 0, 5));
 
 
 function setGravity() {
@@ -255,3 +357,9 @@ function setFriction() {
     friction = parseFloat(document.getElementById("inputFriction").value);
     document.getElementById("setFriction").innerHTML = `Set Friction (Currently: ${friction})`;
 }
+
+canvas.addEventListener('mousemove', function(event) {
+    const rect = canvas.getBoundingClientRect();
+    mouseX = event.clientX - rect.left;
+    mouseY = event.clientY - rect.top;
+});
